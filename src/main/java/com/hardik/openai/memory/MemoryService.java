191a359a1453e3cd5ad8d4155af23e5e97a5414b
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-11-26 16:06:32 +0530

    add system instructions in memory service

--- src/main/java/com/hardik/openai/memory/MemoryService.java
+++ src/main/java/com/hardik/openai/memory/MemoryService.java
@@ -3,6 +3,7 @@ package com.hardik.openai.memory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +11,10 @@ public class MemoryService {
 
     private final ChatClient chatClient;
 
-    public MemoryService(ChatClient.Builder builder, ChatMemory chatMemory) {
+    public MemoryService(ChatClient.Builder builder, ChatMemory chatMemory, @Value("${sysInstructions}") String sysInstructions) {
         this.chatClient = builder
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultSystem(sysInstructions)
                 .build();
     }
 
